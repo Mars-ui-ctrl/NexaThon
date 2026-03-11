52766218a710cd8cce9394e74bdbb07f70a52f6f
master <jitansaw0416@gmail.com>
2026-03-11 12:25:26 +0530

    first commit

---
+++ biteshare/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './assets/logo_no_bg.png';
+function App() {
+  
+
+  return (
+    <>
+      <div className="app-container">
+      <header className="navbar">
+        <h1>BiteShare 🌿</h1>
+        <p>Zero Waste. Zero Hunger.</p>
+      </header>
+      </div>
+    </>
+  )
+}
+
+export default App
